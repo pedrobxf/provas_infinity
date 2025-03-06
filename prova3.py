90486abc23b08284683d0212a6795fd372437e82
pedrobxf <56743069+pedrobxf@users.noreply.github.com>
2025-03-05 23:20:48 -0300

    Add files via upload

---
+++ prova3.py
@@ -0,0 +1,14 @@
+senha = 7
+cont = 0
+print("-----JOGO DOS NÚMEROS-----")
+print("Olá, seja bem vindo ao jogo dos números. Para vencer você deve adivinhar o número que pensei entrei 1 e 10. Você tem 3 tentativas, boa sorte!")
+while cont < 3:
+    tentativa = int(input("Qual número estou pensando? "))
+    if tentativa == senha:
+        print("Parabéns, você acertou, ótimo jogo!")
+        break
+    else:
+        print("Número errado, tente novamente, você consegue!")
+        cont += 1
+if cont == 3:
+    print("Que pena, suas chances acabaram e você perdeu! O número era o 7.")
