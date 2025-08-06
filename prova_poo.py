ddcc2020ccf488b7f2c7b1af370826d8782356e1
pedrobxf <56743069+pedrobxf@users.noreply.github.com>
2025-08-06 05:04:01 -0300

    Add files via upload

---
+++ prova_poo.py
@@ -0,0 +1,25 @@
+class Animal:
+    def __init__(self):
+        return
+    def falar(self):
+        print("Este animal faz um som genérico.")
+
+class Cachorro:
+    def __init__(self):
+        return
+    def falar(self):
+        print("O cachorro está latindo.")
+
+class Gato:
+    def __init__(self):
+        return
+    def falar(self):
+        print("O gato está miando.")
+
+animal = Animal()
+cachorro = Cachorro()
+gato = Gato()
+
+animal.falar()
+cachorro.falar()
+gato.falar()
